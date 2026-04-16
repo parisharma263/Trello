975bd30d8b8cd321af15f21ab6f570cb0513314f
parisharma263 <157444357+parisharma263@users.noreply.github.com>
2026-04-16 23:53:18 +0530

    Update login API endpoint to production URL

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/login', { email, password });
+      const res = await axios.post('https://trellooo.onrender.com/login', { email, password });
       if (res.data.id) {
         // Store user normally using localStorage
         localStorage.setItem('user', JSON.stringify(res.data));
